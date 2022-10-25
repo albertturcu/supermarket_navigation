ac233171b0abbc13dd798387223d2397d12f63fc
chrisgit98 <90904750+chrisgit98@users.noreply.github.com>
2022-10-25 11:37:33 +0200

    search

--- storage/models/product.py
+++ storage/models/product.py
@@ -1,34 +1,14 @@
 import sqlite3, csv
+def search_function(word):
 
-conn = sqlite3.connect('super_market_database.db')
+    w = word
+    conn = sqlite3.connect('super_market_database.db')
+    c = conn.cursor()
+    data = c.execute("SELECT node FROM layout WHERE category LIKE '%'||?||'%'",(w,) )
+    #print( "The node is:", data.fetchall())
 
-c = conn.cursor()
+    result = data.fetchall()
+    tuple = result[0]
+    return tuple[0]
 
-data = c.execute("SELECT node FROM layout WHERE category = 'milk\n' ")
-print( "The node is:", data.fetchall())
-
-
-# c.execute("""CREATE TABLE layout
-#         (
-#             node text,
-#             category text
-#             )""")
-
-
-
-
-# f=open('data_groceries.csv')
-# insert_sql = "insert into groceries45678878 values (?, ?, ?, ?)"
-# for row in str(csv.reader(f)):
-#     c.execute(insert_sql, row)
-
-
-
-# with open('data\layout\layout.csv', 'r') as file:
-#     no_records = 0
-#     for row in file:
-#         c.execute("INSERT INTO layout(node, category) VALUES (?,?)", row.split(';'))
-#         conn.commit()
-#         no_records += 1
-# conn.close()
-# print('\n{} Records transferred'.format(no_records))
+print(search_function('milk')) 
