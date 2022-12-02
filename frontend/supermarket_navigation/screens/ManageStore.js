b1ae72b4b4a1bd6f56dc21dacb4b5324d57dcd86
kubinoes <j.schovanec@protonmail.com>
2022-12-02 19:36:10 +0100

    add functionality to input fields add POST request to create product

--- frontend/supermarket_navigation/screens/ManageStore.js
+++ frontend/supermarket_navigation/screens/ManageStore.js
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { Text, SafeAreaView, Keyboard, TouchableWithoutFeedback, ScrollView } from 'react-native'
-import { TextInput } from 'react-native-paper'
+import { Text, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
 import DropDownPicker from 'react-native-dropdown-picker'
 
+class Category {
+    constructor(label, value) {
+        this.label = label
+        this.value = value
+    }
+}
+
+class Product {
+    constructor(name, brand, price, category) {
+        this.name = name
+        this.brand = brand
+        this.price = price
+        this.category = category
+    }
+}
+
 export default function ManageStore() {
+    // input state
+    const [name, setName] = useState(null)
+    const [brand, setBrand] = useState(null)
+    const [price, setPrice] = useState(null)
+    // category dropdown state
     const [open, setOpen] = useState(false)
-    const [value, setValue] = useState(null)
-    const [items, setItems] = useState([
-        {label: 'Garden', value: 'garden'},
-        {label: 'Home', value: 'home'}
-    ])
+    const [category, setCategory] = useState(null)
+    const [items, setItems] = useState([])
+
+
 
     useEffect(() => {
         fetchCategories()
@@ -18,12 +38,35 @@ export default function ManageStore() {
     const fetchCategories = async () => {
         try {
             const response = await fetch('https://supermarket-navigation.herokuapp.com/category')
+            console.log(response)
             const json = await response.json()
+            console.log(json)
             const data = await json.data
             const modifiedData = data.map((value) => {
-                return {label: value[0].toUpperCase() + value.substr(1), value: value}
+                // capitalize first letter of the category name
+                let label = value[0].toUpperCase() + value.substr(1)
+                // get rid of the underscore from the category name for label in dropdown
+                let modifiedLabel = label.replace(/_/g, ' ')
+                return new Category(modifiedLabel, value)
             })
             setItems(modifiedData)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const addProduct = async () => {
+        try {
+            const payload = new Product(name, brand, price, category)
+            const response = await fetch('https://supermarket-navigation.herokuapp.com/product', {
+                method: 'POST',
+                header: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+            console.log(response)
         } catch (error) {
             console.error(error)
         }
@@ -36,27 +79,32 @@ export default function ManageStore() {
                 <Text>Product Name</Text>
                 <TextInput 
                     placeholder='Product Name'
+                    value={name}
+                    onChangeText={(input) => setName(input)}
                 />
                 <Text>Brand</Text>
                 <TextInput 
                     placeholder='Brand'
+                    value={brand}
+                    onChangeText={(input) => setBrand(input)}
                 />
                 <Text>Price</Text>
                 <TextInput 
                     placeholder='Price'
+                    value={price}
+                    onChangeText={(input) => setPrice(input)}
                 />
             
                 <Text>Category</Text>
                 <DropDownPicker 
                     open={open}
-                    value={value}
+                    value={category}
                     items={items}
                     setOpen={setOpen}
-                    setValue={setValue}
+                    setValue={setCategory}
                     setItems={setItems}
-                    keyboardShouldPersistTaps='handled'
-                    horizontal
                 />
+                <Button onPress={() => addProduct()}>Add Product</Button>
             </SafeAreaView>
         </TouchableWithoutFeedback>
     )
