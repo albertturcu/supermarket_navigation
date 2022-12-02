93615224cf591486e3a8e026caa8c640ed192d32
kubinoes <j.schovanec@protonmail.com>
2022-12-02 21:31:46 +0100

    clean up the look of the add product page

--- frontend/supermarket_navigation/screens/ManageStore.js
+++ frontend/supermarket_navigation/screens/ManageStore.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Text, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
+import { Text, SafeAreaView, Keyboard, TouchableWithoutFeedback, View, StyleSheet } from 'react-native'
+import { Button, TextInput, Drawer, Divider } from 'react-native-paper'
 import DropDownPicker from 'react-native-dropdown-picker'
 
 class Category {
@@ -29,8 +29,6 @@ export default function ManageStore() {
     const [category, setCategory] = useState(null)
     const [items, setItems] = useState([])
 
-
-
     useEffect(() => {
         fetchCategories()
     }, [])
@@ -38,13 +36,13 @@ export default function ManageStore() {
     const fetchCategories = async () => {
         try {
             const response = await fetch('https://supermarket-navigation.herokuapp.com/category')
-            console.log(response)
-            const json = await response.json()
-            console.log(json)
-            const data = await json.data
+            // const json = await response.json()
+            // const data = await json.data
+            // TODO mock data while api is down
+            const data = ["seafood", 'milk', 'auto_moto']
             const modifiedData = data.map((value) => {
                 // capitalize first letter of the category name
-                let label = value[0].toUpperCase() + value.substr(1)
+                let label = value[0].toUpperCase() + value.substring(1)
                 // get rid of the underscore from the category name for label in dropdown
                 let modifiedLabel = label.replace(/_/g, ' ')
                 return new Category(modifiedLabel, value)
@@ -75,37 +73,70 @@ export default function ManageStore() {
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <SafeAreaView style={{flex:1}}>
-                <Text>Add Product</Text>
-                <Text>Product Name</Text>
-                <TextInput 
-                    placeholder='Product Name'
-                    value={name}
-                    onChangeText={(input) => setName(input)}
-                />
-                <Text>Brand</Text>
-                <TextInput 
-                    placeholder='Brand'
-                    value={brand}
-                    onChangeText={(input) => setBrand(input)}
-                />
-                <Text>Price</Text>
-                <TextInput 
-                    placeholder='Price'
-                    value={price}
-                    onChangeText={(input) => setPrice(input)}
-                />
-            
-                <Text>Category</Text>
-                <DropDownPicker 
-                    open={open}
-                    value={category}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setCategory}
-                    setItems={setItems}
-                />
-                <Button onPress={() => addProduct()}>Add Product</Button>
+                <View style={styles.optionsContainer}>
+                    <Button style={styles.optionsButton}>Add Product</Button>
+                    <Button style={styles.optionsButton}>Update Product</Button>
+                </View>
+                <Divider />
+                <View style={{margin: 10}}>
+                    <Text style={styles.input}>Product Name</Text>
+                    <TextInput 
+                        placeholder='Product Name'
+                        value={name}
+                        onChangeText={(input) => setName(input)}
+                        style={styles.input}
+                    />
+                    <Text style={styles.input}>Brand</Text>
+                    <TextInput 
+                        placeholder='Brand'
+                        value={brand}
+                        onChangeText={(input) => setBrand(input)}
+                        style={styles.input}
+                    />
+                    <Text style={styles.input}>Price</Text>
+                    <TextInput 
+                        placeholder='Price'
+                        value={price}
+                        onChangeText={(input) => setPrice(input)}
+                        style={styles.input}
+                    />
+                    <Text style={styles.input}>Category</Text>
+                    <DropDownPicker 
+                        open={open}
+                        value={category}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setCategory}
+                        setItems={setItems}
+                    />
+                    <Button onPress={() => addProduct()} style={styles.submitButton}>Add Product</Button>
+                </View>
             </SafeAreaView>
         </TouchableWithoutFeedback>
     )
 }
+
+const styles = StyleSheet.create({
+    optionsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    optionsButton: {
+        padding: 10
+    },
+    inputAndButtonContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly'
+    },
+    inputContainer: {
+        padding: 20
+    },
+    input: {
+        marginBottom: 10
+    },
+    submitButton: {
+        padding: 20
+    }
+})
